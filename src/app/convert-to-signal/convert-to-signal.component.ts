e4c3d18432fc7a8f64dfd3631fd903473ac3b687
Wei Tian <wtian@omnissa.com>
2025-11-18 15:25:49 +0800

    uncomment the needed part

--- src/app/convert-to-signal/convert-to-signal.component.ts
+++ src/app/convert-to-signal/convert-to-signal.component.ts
@@ -1,19 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SimpleUser } from '../model/user';
+import { Field, form, required, submit } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-convert-to-signal',
   standalone: true,
   imports: [
     CommonModule,
+    Field,
     ReactiveFormsModule,
   ],
   templateUrl: './convert-to-signal.component.html',
   styleUrls: ['./convert-to-signal.component.scss'],
 })
 export class ConvertToSignalComponent {
+  public user = signal<SimpleUser>({
+    firstName: '',
+  });
+
+  public userForm = form(this.user, (path) => {
+    required(path.firstName, { message: 'First Name is required' });
+  });
+
   onSubmit(event: Event) {
     event.preventDefault();
+    submit(this.userForm, (form) => {
+      console.log('Form Submitted:', form().value());
+      return Promise.resolve([
+        {
+          kind: 'server',
+          field: form.firstName,
+          message: 'Server-side error on First Name',
+        }
+      ]);
+    });
   }
 }
--- src/app/signal-forms-poc/signal-forms-poc.component.ts
+++ src/app/signal-forms-poc/signal-forms-poc.component.ts
@@ -41,90 +41,90 @@ const ageSchema: Schema<number> = schema((path) => {
 });
 
 const commonSchema: Schema<User> = schema<User>((path) => {
-  // apply(path.firstName, nameSchema);
-  // apply(path.lastName, nameSchema);
-  // apply(path.age, ageSchema);
-  // applyWhen(
-  //   path,
-  //   (ctx) => ctx.valueOf(path.notified) === true,
-  //   emailNotifiedSchema
-  // );
-  // disabled(path.email, (ctx) =>
-  //   ctx.valueOf(path.notified) === false
-  //     ? 'Email is not required when notified is disabled!'
-  //     : false
-  // );
+  apply(path.firstName, nameSchema);
+  apply(path.lastName, nameSchema);
+  apply(path.age, ageSchema);
+  applyWhen(
+    path,
+    (ctx) => ctx.valueOf(path.notified) === true,
+    emailNotifiedSchema
+  );
+  disabled(path.email, (ctx) =>
+    ctx.valueOf(path.notified) === false
+      ? 'Email is not required when notified is disabled!'
+      : false
+  );
 
-  // applyEach(path.preferences, (path) => {
-  //   apply(path, nameSchema);
-  // });
+  applyEach(path.preferences, (path) => {
+    apply(path, nameSchema);
+  });
 
-  // validate(path.preferences, (ctx) => {
-  //   console.log('Validating preferences...');
-  //   const preferences = ctx.valueOf(path.preferences);
-  //   if (!preferences || preferences?.length === 0) {
-  //     return {
-  //       kind: 'custom',
-  //       message: 'At least one preference is required',
-  //     };
-  //   }
-  //   return null;
-  // });
+  validate(path.preferences, (ctx) => {
+    console.log('Validating preferences...');
+    const preferences = ctx.valueOf(path.preferences);
+    if (!preferences || preferences?.length === 0) {
+      return {
+        kind: 'custom',
+        message: 'At least one preference is required',
+      };
+    }
+    return null;
+  });
 
-  // validateAsync(path.email, {
-  //   params: (ctx) => ({
-  //     value: ctx.value(),
-  //   }),
-  //   factory: (params) => {
-  //     return rxResource({
-  //       params,
-  //       stream: (p) => {
-  //         return validateEmailNotInUse(p.params.value);
-  //       },
-  //     });
-  //   },
-  //   onError: (result, ctx) => {
-  //     if (!result) {
-  //       return null;
-  //     }
-  //     return {
-  //       kind: 'custom',
-  //       field: ctx.field,
-  //       message: 'Email is already in use',
-  //     };
-  //   },
-  //   onSuccess: (result, ctx) => {
-  //     return null;
-  //   },
-  // });
+  validateAsync(path.email, {
+    params: (ctx) => ({
+      value: ctx.value(),
+    }),
+    factory: (params) => {
+      return rxResource({
+        params,
+        stream: (p) => {
+          return validateEmailNotInUse(p.params.value);
+        },
+      });
+    },
+    onError: (result, ctx) => {
+      if (!result) {
+        return null;
+      }
+      return {
+        kind: 'custom',
+        field: ctx.field,
+        message: 'Email is already in use',
+      };
+    },
+    onSuccess: (result, ctx) => {
+      return null;
+    },
+  });
 
-  // validateTree(path, (ctx) => {
-  //   const invalidName =
-  //     ctx.valueOf(path.firstName) === ctx.valueOf(path.lastName) &&
-  //     ctx.valueOf(path.firstName) !== '';
-  //   if (!invalidName) {
-  //     return null;
-  //   }
-  //   return {
-  //     kind: 'custom',
-  //     field: ctx.field.lastName,
-  //     message: 'First name and last name cannot be the same',
-  //   };
-  // });
+  validateTree(path, (ctx) => {
+    const invalidName =
+      ctx.valueOf(path.firstName) === ctx.valueOf(path.lastName) &&
+      ctx.valueOf(path.firstName) !== '';
+    if (!invalidName) {
+      return null;
+    }
+    return {
+      kind: 'custom',
+      field: ctx.field.lastName,
+      message: 'First name and last name cannot be the same',
+    };
+  });
 
-  // apply(path.preferences, (path) => {
-  //   debounce(path, 2000);
-  // });
+  apply(path.preferences, (path) => {
+    debounce(path, 2000);
+  });
 
-  // apply(path.locationData, (location) => {
-  //   required(location.country, { message: 'Country is required' });
-  //   required(location.city, { message: 'City is required' });
-  //   disabled(location.city, (ctx) =>
-  //     ctx.valueOf(location.country) === 'USA'
-  //       ? 'City is not required for USA'
-  //       : false
-  //   );
-  // });
+  apply(path.locationData, (location) => {
+    required(location.country, { message: 'Country is required' });
+    required(location.city, { message: 'City is required' });
+    disabled(location.city, (ctx) =>
+      ctx.valueOf(location.country) === 'USA'
+        ? 'City is not required for USA'
+        : false
+    );
+  });
 });
 
 function validateEmailNotInUse(email: string) {
